54bf433ddf878e34fab35b87c565d0abb0f17d73
Ashish11-19 <ashishpathak0719@gmail.com>
2025-08-14 17:23:28 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "emi-calculator.GharPadharo.com",
+  title: "Emi Calculator",
   description: "EMI Calculator by Ghar Padharo",
  
 };
